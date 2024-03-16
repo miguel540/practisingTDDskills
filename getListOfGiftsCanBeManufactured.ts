8f89bd5b06a718f3f4d8a36ec65c6194bbbfd2bd
miguel540 <miguelruizperezdeciriza@gmail.com>
2024-03-16 09:01:03 +0100

    Acabado el test dia dos del aviento

--- getListOfGiftsCanBeManufactured.ts
+++ getListOfGiftsCanBeManufactured.ts
@@ -1,4 +1,53 @@
-export const getListOfGiftsCanBeManufactured = (giftToMade:string[]):void => {
-       if (!Array.isArray(giftToMade)) { throw new Error('giftToMade must be a array');}
-       if (!giftToMade.every((elementOfArray) => typeof elementOfArray ==='string')){ throw new Error('All of giftToMade"s elements must be a string');}
+const isAllElementsAString=(array:string[]):boolean=>{
+       return array.every((elementOfArray) => typeof elementOfArray ==='string')
+}
+
+interface IToy{
+       toy:string;
+       canMakeIt:boolean;
+}
+
+const proccesingToy = (str:string, c:string) => str.replace(new RegExp(`[${c}]`, "gi"), "");
+
+const canMakeIt = (toyToProcessing:string,materials:string):boolean =>{
+       for (const material of materials)
+       {
+              toyToProcessing= proccesingToy(toyToProcessing,material);
+       }
+       return toyToProcessing.length===0?true:false;
+}
+
+const validateParameters= (giftToMade:string[],materials:string) =>
+{
+       if (!Array.isArray(giftToMade)||!isAllElementsAString(giftToMade)) 
+       { throw new Error("The first parameter of the function is not an array or one of its elements is not a string.");}
+       
+       if (typeof materials!='string')
+              { throw new Error("The second parameter of the function is not a string.");}
+
+}
+
+const createToy = (toyTomake:string,materials:string)=>
+{
+       return  {
+                     toy:toyTomake,
+                     canMakeIt:canMakeIt(toyTomake,materials)
+              }
+}
+
+export const getListOfGiftsCanBeManufactured = (giftToMade:string[],materials:string):IToy[] => {
+       
+       validateParameters(giftToMade,materials);       
+    
+       // const finalList:IToy[]=[];
+
+       // giftToMade.forEach(toyToProcedure=>{
+       //        finalList.push(
+       //               createToy(toyToProcedure,materials)
+       //               );
+       // });
+       
+       // return finalList;
+
+       return giftToMade.map(toyToProcedure => createToy(toyToProcedure, materials));
 }
--- test/getListOfGiftsCanBeManufactured.test.ts
+++ test/getListOfGiftsCanBeManufactured.test.ts
@@ -20,34 +20,37 @@ const materials = 'tronesa'
 
 import { describe, it, expect } from 'vitest';
 import { getListOfGiftsCanBeManufactured } from '../getListOfGiftsCanBeManufactured';
+//import {IToy} from '../getListOfGiftsCanBeManufactured';
 
 
 describe('getListOfGiftsCanBeManufactured', () =>
  {
-    it('Must be a function <ser una función', () :void=> 
-         {
-             expect(typeof getListOfGiftsCanBeManufactured).toBe('function'); 
-     });
 
-     it('If parameter giftToMade is not a array launch a exception with the message -> "giftToMade must be a array"', () :void=>
-         {
-            expect(():void=>getListOfGiftsCanBeManufactured()).toThrow('giftToMade must be a array');
-            expect(():void=>getListOfGiftsCanBeManufactured(1)).toThrow('giftToMade must be a array');
-            expect(():void=>getListOfGiftsCanBeManufactured('cadena')).toThrow('giftToMade must be a array');
-            expect(():void=>getListOfGiftsCanBeManufactured(true)).toThrow('giftToMade must be a array');
-            expect(():void=>getListOfGiftsCanBeManufactured(NaN)).toThrow('giftToMade must be a array');
-            expect(():void=>getListOfGiftsCanBeManufactured(undefined)).toThrow('giftToMade must be a array');
-            expect(():void=>getListOfGiftsCanBeManufactured(null)).toThrow('giftToMade must be a array');
-    });
-
-    it('All giftToMade"s elements must be a string , if not launch a exception with the message -> "All of giftToMade"s elements must be a string"', () :void=> 
-    {
-        expect(():void=>getListOfGiftsCanBeManufactured([1,2,3])).toThrow('All of giftToMade"s elements must be a string');
-        expect(():void=>getListOfGiftsCanBeManufactured(['1','2',NaN])).toThrow('All of giftToMade"s elements must be a string');
-        expect(():void=>getListOfGiftsCanBeManufactured([undefined,'2','3'])).toThrow('All of giftToMade"s elements must be a string');
-        expect(():void=>getListOfGiftsCanBeManufactured([true,'2','3'])).toThrow('All of giftToMade"s elements must be a string');
-        expect(():void=>getListOfGiftsCanBeManufactured(['1',{a:1,b:2},'3'])).toThrow('All of giftToMade"s elements must be a string');
-        expect(():void=>getListOfGiftsCanBeManufactured(['1',[{a:1,b:2}],'3'])).toThrow('All of giftToMade"s elements must be a string');
-    });
+        it('the function receives a first parameter which is an array where all its elements are strings.', () :void=>{
+         expect(():void=>getListOfGiftsCanBeManufactured()).toThrow('The first parameter of the function is not an array or one of its elements is not a string.');
+         expect(():void=>getListOfGiftsCanBeManufactured(1)).toThrow('The first parameter of the function is not an array or one of its elements is not a string.');
+         expect(():void=>getListOfGiftsCanBeManufactured(null)).toThrow('The first parameter of the function is not an array or one of its elements is not a string.');
+         expect(():void=>getListOfGiftsCanBeManufactured(undefined)).toThrow('The first parameter of the function is not an array or one of its elements is not a string.');
+         expect(():void=>getListOfGiftsCanBeManufactured(true)).toThrow('The first parameter of the function is not an array or one of its elements is not a string.');
+         expect(():void=>getListOfGiftsCanBeManufactured('abcd')).toThrow('The first parameter of the function is not an array or one of its elements is not a string.');
+         expect(():void=>getListOfGiftsCanBeManufactured([1,'2'])).toThrow('The first parameter of the function is not an array or one of its elements is not a string.');
+        });
     
+        it('the function receives a second parameter which is a string.', () :void=>{
+            expect(():void=>getListOfGiftsCanBeManufactured(['tren','coche'],2)).toThrow('The second parameter of the function is not a string.');
+            expect(():void=>getListOfGiftsCanBeManufactured(['tren','coche'],null)).toThrow('The second parameter of the function is not a string.');
+            expect(():void=>getListOfGiftsCanBeManufactured(['tren','coche'],undefined)).toThrow('The second parameter of the function is not a string.');
+            expect(():void=>getListOfGiftsCanBeManufactured(['tren','coche'],true)).toThrow('The second parameter of the function is not a string.');
+            expect(():void=>getListOfGiftsCanBeManufactured(['tren','coche'],['abcde'])).toThrow('The second parameter of the function is not a string.');
+           });
+       
+    
+        it('The function returns  canMakeIt:false when there are not materials to make the toy', () :void=>{
+            expect(getListOfGiftsCanBeManufactured(['tren','coche'],'ahncdeo')).toEqual([{ toy:'tren',canMakeIt: false }, { toy:'coche',canMakeIt: true }]);
+            expect(getListOfGiftsCanBeManufactured(['tren', 'oso', 'pelota'],'tronesa')).toEqual([{ toy:'tren',canMakeIt: true }, { toy:'oso',canMakeIt: true },{ toy:'pelota',canMakeIt: false }]);
+            
+        });
+
+        
+
 })
